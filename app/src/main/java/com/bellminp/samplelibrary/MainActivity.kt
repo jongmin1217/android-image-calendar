ee49fdd6c532474c626f1197f48245d789ecab56
jongmin1217 <syj408886@gmail.com>
2022-01-07 14:19:23 +0900

    upload test

--- app/src/main/java/com/bellminp/samplelibrary/MainActivity.kt
+++ app/src/main/java/com/bellminp/samplelibrary/MainActivity.kt
@@ -2,10 +2,16 @@ package com.bellminp.samplelibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.bellminp.imagecalendar.ToastClass
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.tv_test).setOnClickListener {
+            ToastClass.testToast(this,"hello world!!")
+        }
     }
 }
