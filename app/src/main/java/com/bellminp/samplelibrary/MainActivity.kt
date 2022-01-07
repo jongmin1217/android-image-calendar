0e5ef1548ff2ef3cfaa7db4f089e753767d130bd
jongmin1217 <syj408886@gmail.com>
2022-01-07 13:51:48 +0900

    first commit

---
+++ app/src/main/java/com/bellminp/samplelibrary/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bellminp.samplelibrary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
